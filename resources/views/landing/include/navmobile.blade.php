9c0202653706643390d2e8add4a6b95ec535a462
dikaantoniputra <dikahanggara2@gmail.com>
2024-10-09 22:08:18 +0700

    update

--- resources/views/landing/include/navmobile.blade.php
+++ resources/views/landing/include/navmobile.blade.php
@@ -32,14 +32,14 @@
                             <li class="menu-itemmenu-item-home current-menu-item page_item page-item-6208 current_page_item ">
                                 <a href="index.html" aria-current="page">Home</a>
                             </li>
-                            <li class="menu-item"><a href="pengembangan-pirngadi">SMA PIRNGADI</a></li>
-                            <li class="menu-item"><a href="pengembangan-pirngadi">SMP PIRNGADI</a></li>
+                            <li class="menu-item"><a href="https://sma-kristen.pirngadi.sch.id/">SMA PIRNGADI</a></li>
+                            <li class="menu-item"><a href="https://smp-kristen.pirngadi.sch.id/">SMP PIRNGADI</a></li>
                             <li class="menu-item"><a href="https://sd-kristen.pirngadi.sch.id/">SD PIRNGADI</a></li>
                             <li class="menu-item"><a href="https://tk-kristen.pirngadi.sch.id/">TK PIRNGADI</a></li>
                             <li class="menu-item"><a href="https://daycare-montessori.pirngadi.sch.id/">DAYCARE</a></li>
                             <li class="menu-item"><a href="https://ybppkpirngadi.or.id/">YBPPK PIRNGADI</a></li>
                             <li class="menu-itemmenu-item-has-children ">
-                                <a href="blog-full-right-sidebar-with-frame.html">E-PIRNGADI</a>
+                                <a href="#">E-PIRNGADI</a>
                                 <ul class="sub-menu">
                                     <li class="menu-item menu-item-has-children"><a href="blog-full-right-sidebar.html">Sistem Absensi</a>
                                         <ul class="sub-menu">
@@ -50,12 +50,21 @@
                                            
                                         </ul>
                                     </li>
-                                    <li class="menu-item menu-item-has-children"><a href="blog-grid-3-columns-no-space.html">PPDB Pirngadi</a>
+                                    <li class="menu-item menu-item-has-children"><a href="#">PPDB Pirngadi</a>
                                         <ul class="sub-menu">
-                                            <li class="menu-item"><a href="pengembangan-pirngadi">Pendaftaran SMA</a></li>
-                                            <li class="menu-item"><a href="pengembangan-pirngadi">Pendaftaran SMP</a></li>
-                                            <li class="menu-item "><a href="pengembangan-pirngadi">Pendaftaran SD</a></li>
-                                            <li class="menu-item "><a href="#">Pendaftaran TK</a></li>
+                                            <li class="menu-item">
+                                                <a href="https://wa.me/6281803977779?text=Cara%20pendaftaran%20PPDB%20untuk%20SMA" style="color: inherit;">Pendaftaran SMA</a>
+                                            </li>
+                                            <li class="menu-item">
+                                                <a href="https://wa.me/6281803977779?text=Cara%20pendaftaran%20PPDB%20untuk%20SMP" style="color: inherit;">Pendaftaran SMP</a>
+                                            </li>
+                                            <li class="menu-item">
+                                                <a href="https://wa.me/6281803977779?text=Cara%20pendaftaran%20PPDB%20untuk%20SD" style="color: inherit;">Pendaftaran SD</a>
+                                            </li>
+                                            <li class="menu-item">
+                                                <a href="https://wa.me/6281803977779?text=Cara%20pendaftaran%20PPDB%20untuk%20TK" style="color: inherit;">Pendaftaran TK</a>
+                                            </li>
+                                            
                                         </ul>
                                     </li>
                                   
